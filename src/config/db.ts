7ef18368f39c2ea880f516f84ff5461d2d657b83
jose230203 <jose2302037d@gmail.com>
2026-07-30 14:59:13 -0600

    feat: estructura inicial de arquitectura del proyecto

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,4 +1,3 @@
-// src/config/db.ts o prisma/client.ts
 
 import { PrismaClient } from '@prisma/client';
 
@@ -7,7 +6,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['query', 'error', 'warn'], // Te muestra en la consola de Express los queries reales que hace la app
+    log: ['query', 'error', 'warn'],
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
